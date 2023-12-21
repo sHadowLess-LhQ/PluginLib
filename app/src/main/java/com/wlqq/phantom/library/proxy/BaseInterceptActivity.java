5e1d35b336219965cccda63cafe10156138d0f6b
lihq <1206863318@qq.com>
2023-12-21 10:49:07 +0800

    :art: 优化BaseInterceptActivity

--- app/src/main/java/com/wlqq/phantom/library/proxy/BaseInterceptActivity.java
+++ app/src/main/java/com/wlqq/phantom/library/proxy/BaseInterceptActivity.java
@@ -35,16 +35,11 @@ import io.reactivex.disposables.Disposable;
  * @param <T>  the type 传递数据类型
  * @author sHadowLess
  */
-public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends PluginInterceptActivity implements ObservableOnSubscribe<T>, Observer<T>, View.OnClickListener {
+public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends PluginInterceptActivity implements View.OnClickListener {
     /**
      * 视图绑定
      */
     private VB bind = null;
-
-    /**
-     * The Is only complete.
-     */
-    private volatile boolean isOnlyComplete = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +49,13 @@ public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends P
         }
         super.onCreate(savedInstanceState);
         initBindView();
-        initListener();
-        initPermissionData();
+        initViewListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initPermissionAndInitData();
     }
 
     @Override
@@ -67,105 +67,11 @@ public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends P
     }
 
     @Override
-    public void subscribe(@NonNull ObservableEmitter<T> emitter) throws Exception {
-        initData(new InitDataCallBack<T>() {
-            @Override
-            public void initSuccessViewWithData(@NonNull T t) {
-                isOnlyComplete = false;
-                emitter.onNext(t);
-                emitter.onComplete();
-            }
-
-            @Override
-            public void initSuccessViewWithOutData() {
-                isOnlyComplete = true;
-                emitter.onComplete();
-            }
-
-            @Override
-            public void initFailView(Throwable e) {
-                emitter.onError(e);
-            }
-        });
-    }
-
-    @Override
-    public void onSubscribe(@NonNull Disposable d) {
-
-    }
-
-    @Override
-    public void onNext(@NonNull T mData) {
-        initSuccessView(mData);
-    }
-
-    @Override
-    public void onError(@NonNull Throwable e) {
-        initFailView(e);
-    }
-
-    @Override
-    public void onComplete() {
-        if (isOnlyComplete) {
-            initSuccessView(null);
-        }
-    }
-
-    @Override
     public void onClick(View v) {
         if (!ClickUtils.isFastClick()) {
             click(v);
         }
     }
-
-    /**
-     * 需要申请的权限
-     *
-     * @return the 权限组
-     */
-    @Nullable
-    protected abstract String[] permissions();
-
-    /**
-     * 设置绑定视图
-     *
-     * @return the 视图
-     */
-    @NonNull
-    protected abstract String setBindViewClassName();
-
-    /**
-     * 初始化监听
-     */
-    protected abstract void initListener();
-
-    /**
-     * 初始化数据
-     *
-     * @param callBack the call back
-     */
-    protected abstract void initData(@NonNull InitDataCallBack<T> callBack);
-
-    /**
-     * 初始化视图
-     *
-     * @param data the 数据表
-     */
-    protected abstract void initSuccessView(@Nullable T data);
-
-    /**
-     * Init fail view.
-     *
-     * @param e the e
-     */
-    protected abstract void initFailView(@Nullable Throwable e);
-
-    /**
-     * 点击
-     *
-     * @param v the v
-     */
-    protected abstract void click(@NonNull View v);
 
     /**
      * 反射实例化ViewBinding
@@ -174,7 +80,7 @@ public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends P
      */
     protected VB inflateView() {
         try {
-            return (VB) ViewBindingUtils.inflate(setBindViewClassName(), getLayoutInflater());
+            return ViewBindingUtils.inflate(setBindViewClass().getName(), getLayoutInflater());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -201,15 +107,17 @@ public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends P
     }
 
     /**
-     * 设置调度器
-     *
-     * @return the scheduler
+     * 初始化权限
      */
-    protected Scheduler[] setScheduler() {
-        return new Scheduler[]{
-                AndroidSchedulers.mainThread(),
-                AndroidSchedulers.mainThread()
-        };
+    private void initPermissionAndInitData() {
+        String[] permissions = permissions();
+        if (null == permissions || permissions.length == 0) {
+            initObject();
+            initBindDataLister();
+            initData();
+            return;
+        }
+        initPermission(permissions);
     }
 
     /**
@@ -230,78 +138,46 @@ public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends P
     protected void dealPermission(String[] permissions, PermissionCallBack callBack) {
         final List<String> disagree = new ArrayList<>();
         final List<String> ban = new ArrayList<>();
-        PermissionUtils.getPermissionObservable(this, this, permissions)
-                .subscribe(new Observer<Permission>() {
-                               @Override
-                               public void onSubscribe(@NonNull Disposable d) {
+        PermissionUtils.getPermissionObservable(this, this, permissions).subscribe(new Observer<Permission>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
 
-                               }
+            }
 
-                               @Override
-                               public void onNext(@NonNull Permission permission) {
-                                   if (permission.shouldShowRequestPermissionRationale) {
-                                       ban.add(permission.name);
-                                   } else if (!permission.granted) {
-                                       disagree.add(permission.name);
-                                   }
-                               }
+            @Override
+            public void onNext(@NonNull Permission permission) {
+                if (permission.shouldShowRequestPermissionRationale) {
+                    ban.add(permission.name);
+                } else if (!permission.granted) {
+                    disagree.add(permission.name);
+                }
+            }
 
-                               @Override
-                               public void onError(@NonNull Throwable e) {
-                                   Toast.makeText(BaseInterceptActivity.this, "处理权限错误", Toast.LENGTH_SHORT).show();
-                               }
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Toast.makeText(BaseInterceptActivity.this, "处理权限错误", Toast.LENGTH_SHORT).show();
+            }
 
-                               @Override
-                               public void onComplete() {
-                                   if (ban.isEmpty() && disagree.isEmpty()) {
-                                       if (callBack != null) {
-                                           callBack.agree();
-                                       }
-                                       dealDataToView();
-                                   } else if (!ban.isEmpty()) {
-                                       if (callBack != null) {
-                                           callBack.ban(ban);
-                                       }
-                                   } else {
-                                       if (callBack != null) {
-                                           callBack.disagree(disagree);
-                                       }
-                                   }
-                               }
-                           }
-                );
-    }
-
-    /**
-     * 初始化权限
-     */
-    private void initPermissionData() {
-        String[] permissions = permissions();
-        if (null == permissions || permissions.length == 0) {
-            dealDataToView();
-            return;
-        }
-        initPermission(permissions);
-    }
-
-    /**
-     * Deal data.
-     */
-    private void dealDataToView() {
-        Observable.create(this).compose(dealWithThreadMode(setScheduler())).as(RxLife.as(this)).subscribe(this);
-    }
-
-    /**
-     * 初始化数据所在线程
-     *
-     * @param <TF>      the type parameter
-     * @param scheduler the scheduler
-     * @return the 线程模式
-     */
-    private <TF> ObservableTransformer<TF, TF> dealWithThreadMode(Scheduler[] scheduler) {
-        return upstream -> upstream.subscribeOn(scheduler[0])
-                .unsubscribeOn(scheduler[0])
-                .observeOn(scheduler[1]);
+            @Override
+            public void onComplete() {
+                if (ban.isEmpty() && disagree.isEmpty()) {
+                    if (callBack != null) {
+                        callBack.agree();
+                    }
+                    initObject();
+                    initBindDataLister();
+                    initData();
+                } else if (!ban.isEmpty()) {
+                    if (callBack != null) {
+                        callBack.ban(ban);
+                    }
+                } else {
+                    if (callBack != null) {
+                        callBack.disagree(disagree);
+                    }
+                }
+            }
+        });
     }
 
     /**
@@ -310,8 +186,51 @@ public abstract class BaseInterceptActivity<VB extends ViewBinding, T> extends P
     private void initBindView() {
         bind = inflateView();
         if (bind == null) {
-            throw new RuntimeException("视图无法反射初始化，请检查setBindViewClassName传是否入绝对路径或重写自实现inflateView方法");
+            throw new RuntimeException("视图无法反射初始化，请检查setBindViewClassName是否传入正确或重写自实现inflateView方法");
         }
         setContentView(bind.getRoot());
     }
+
+    /**
+     * 需要申请的权限
+     *
+     * @return the 权限组
+     */
+    @Nullable
+    protected abstract String[] permissions();
+
+    /**
+     * 设置绑定视图
+     *
+     * @return the 视图
+     */
+    @NonNull
+    protected abstract Class<VB> setBindViewClass();
+
+    /**
+     * 初始化视图监听
+     */
+    protected abstract void initViewListener();
+
+    /**
+     * 初始化对象
+     */
+    protected abstract void initObject();
+
+    /**
+     * 给视图绑定数据
+     */
+    protected abstract void initBindDataLister();
+
+    /**
+     * 初始化数据
+     */
+    protected abstract void initData();
+
+    /**
+     * 点击
+     *
+     * @param v the v
+     */
+    protected abstract void click(@NonNull View v);
 }
